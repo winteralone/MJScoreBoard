139a788070d7b3f3cd3b5373c5da586c88e71867
winteralone <winteralone@gmail.com>
2014-08-11 22:47:49 +0800

    界面微调

--- MJScoreBoard/MJScoreMainTableCell.m
+++ MJScoreBoard/MJScoreMainTableCell.m
@@ -45,12 +45,12 @@
             [label setTextAlignment:NSTextAlignmentCenter];
             if (i%2 == 1)
             {
-                [label setFont:[UIFont boldSystemFontOfSize:24]];
+                [label setFont:[UIFont boldSystemFontOfSize:26]];
                 [label setTextColor:[UIColor colorWithRed:205/255.f green:1.f blue:205/255.f alpha:1]];
             }
             else
             {
-                [label setFont:[UIFont systemFontOfSize:24]];
+                [label setFont:[UIFont systemFontOfSize:26]];
                 [label setTextColor:[UIColor whiteColor]];
                 
             }
@@ -62,8 +62,9 @@
         [_scoreElementLabel setFont:[UIFont boldSystemFontOfSize:22]];
         [_scoreElementLabel setTextColor:[UIColor whiteColor]];
         [_scoreElementLabel setAdjustsFontSizeToFitWidth:YES];
-        _infoButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-        _infoButton.frame = CGRectMake(TABLE_LEFT_BOUNDARY + cellWidth*8 + SCORE_ELEMENT_LABEL_WIDTH, 0, INFO_BUTTON_WIDTH, CELL_HEIGHT);
+        _infoButton = [[UIButton alloc]initWithFrame:CGRectMake(TABLE_LEFT_BOUNDARY + cellWidth*8 + SCORE_ELEMENT_LABEL_WIDTH, 0, 20, 20)];
+        UIImage *image = [UIImage imageNamed:@"Pencil.png"];
+        [_infoButton setImage: image forState:UIControlStateNormal];
         [_infoButton addTarget:self.superview action:@selector(clickedInfoButton:) forControlEvents:UIControlEventTouchUpInside];
         _infoButton.tintColor = [UIColor colorWithRed:205/255.f green:1.f blue:205/255.f alpha:1];
         [self addSubview:_scoreElementLabel];
@@ -92,7 +93,7 @@
         }
         _scoreElementLabel.frame = CGRectMake(TABLE_LEFT_BOUNDARY + cellWidth * 8, 0, SCORE_ELEMENT_LABEL_WIDTH, CELL_HEIGHT);
         _scoreElementLabel.hidden = NO;
-        _infoButton.frame = CGRectMake(self.bounds.size.width - INFO_BUTTON_WIDTH, 0, INFO_BUTTON_WIDTH, CELL_HEIGHT);
+        _infoButton.frame = CGRectMake(self.bounds.size.width - INFO_BUTTON_WIDTH + 10, 12, 20, 20);
     }
     else // mode == 1
     {
@@ -104,7 +105,7 @@
             label.hidden = NO;
         }
         _scoreElementLabel.hidden = YES;
-        _infoButton.frame = CGRectMake(self.bounds.size.width - INFO_BUTTON_WIDTH, 0, INFO_BUTTON_WIDTH, CELL_HEIGHT);
+        _infoButton.frame = CGRectMake(self.bounds.size.width - INFO_BUTTON_WIDTH + 10, 12, 20, 20);
     }
     _mode = mode;
 }
